809058c9399aa2c838457b582703c013dbcad38d
Atanas Ormanov <atanasormanow@gmail.com>
2023-03-18 01:46:11 +0200

    Modify delimiters for the fields

--- models/game_state.py
+++ models/game_state.py
@@ -1,8 +1,12 @@
 from field import *
+from player import *
 
 class GameState:
-  def __init__(self):
+  # TODO: add current player?
+  def __init__(self, ids):
     f = open('../static/fields.txt', 'r')
-    self.fields = [Field(l.rstrip('\n').split(',')) for l in f.readlines()]
+    self.fields = [Field(l.rstrip('\n').split(';')) for l in f.readlines()]
 
-print([f.rent for f in GameState().fields])
+    ids_and_colors = zip(ids, ['red', 'blue', 'green', 'yellow'])
+    # NOTE: The initial balance is usually 15mil
+    self.players = map(lambda id,col: Player(id, col, 15000, 0), ids_and_colors)
