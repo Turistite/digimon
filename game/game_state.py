8fe6aa2b143936a66423aa00eb6b977d3b19a0ca
Atanas Ormanov <atanasormanow@gmail.com>
2023-03-18 16:43:46 +0200

    Add upgrade function for game state and fix bugs

--- game/game_state.py
+++ game/game_state.py
@@ -10,7 +10,7 @@ class GameState:
             for line in f.readlines()
         ]
 
-        ids_and_colors = zip(ids, PLAYER_COLORS[1:len(ids)])
+        ids_and_colors = zip(ids, PLAYER_COLORS[0:len(ids)])
         self.players = [
             Player(id, color, 1500, 0)
             for id, color in ids_and_colors
@@ -21,8 +21,6 @@ class GameState:
     def dice(self, points):
         curr_player = self.players[self.curr_player]
         curr_field = self.fields[curr_player.position]
-
-        curr_player.position += points
 
         curr_player.move(points, len(self.fields))
 
@@ -38,9 +36,13 @@ class GameState:
 
         # TODO cover case for Status.SPECIAL
 
-
     def upgrade_property(self, prop_ids):
-        return # TODO
+        props_for_upgrade = filter(lambda f: f.id in prop_ids, self.fields)
+
+        # TODO: check if the player owns the whole neighbourhood
+        for p in props_for_upgrade:
+            p.upgrade()
+            self.players[self.curr_player].pay(0.6 * p.price)
 
     def end_turn(self):
         self.curr_player = (self.curr_player + 1) % len(self.players)
